6057bdf4d13efbe4716cd199362f6f3f6218fd7d
Manuel Rueda <rueda.manu@gmail.com>
2024-03-01 16:52:48 +0100

    Source commit

--- pheno-search.py
+++ pheno-search.py
@@ -12,13 +12,20 @@ nlp = spacy.load("en_core_web_sm")
 # Process the natural language query
 query_text = "Alzheimer disease, susceptibility to"
 doc = nlp(query_text)
-
-# Example logic to construct an Elasticsearch query (simplified)
 entities = [ent.text for ent in doc.ents if ent.label_ in ['DISEASE', 'CONDITION']]
-# Assuming 'DISEASE' and 'CONDITION' are custom or pre-defined labels you are interested in
 
-# Simply query
+# Simply queries
 query = {
+    "query": {
+        "term": {
+            "sex.id": {
+                "value": "NCIT:C20197"  # Adjust this value as needed
+            }
+        }
+    }
+}
+
+_query = {
    "_source": ["id"],  # Specify the fields to include in the response
   "query": {
     "nested": {
@@ -50,14 +57,34 @@ es_query = {
     }
 }
 
+# Example logic to construct an Elasticsearch query with fuzzy search
+fs_query = {
+    "query": {
+        "nested": {
+            "path": "diseases",  # Specify the path to the nested object
+            "query": {
+                "match": {
+                    "diseases.diseaseCode.label": {
+                        "query": "Alzheimer disease",  # The search text
+                        "fuzziness": 2,  # Allow for some fuzziness in the match
+                        "prefix_length": 1,
+                        "max_expansions": 100
+                    }
+                }
+            },
+            "score_mode": "avg"  # How to score matches for multiple nested objects
+        }
+    }
+}
+
 # Perform the search with the correctly constructed query
 response = es.search(index="dataset1", body=query)
 #response = es.search(index="dataset1", body=es_query)
+#response = es.search(index="dataset1", body=fs_query)
 
 # Pretty print the response
 print(json.dumps(response, indent=2))
 
 # Extract 'id' from each hit
-ids = [hit['_id'] for hit in response['hits']['hits']]
-
-print(ids)
+#ids = [hit['_id'] for hit in response['hits']['hits']]
+#print(ids)
